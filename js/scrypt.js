a846d6f4b71aba9938e2492d9fbdafba24c17836
Tamir <amorimlds@gmail.com>
2024-05-01 20:02:21 -0300

    Jogo da Memória

---
+++ js/scrypt.js
@@ -0,0 +1,31 @@
+
+//variaveis globais
+const input = document.querySelector('.login__input');
+const button = document.querySelector('.login__button');
+const form = document.querySelector('.login-form');
+
+
+
+
+//Habilita o botão play
+function validaInput ({target}){
+    if(target.value.length > 3 ) {
+        button.removeAttribute('disabled');
+    }
+    else{button.setAttribute('disabled', '');}
+}
+
+function handleSubmit(event) {
+event.preventDefault();
+console.log('logando');
+
+localStorage.setItem('player', input.value);
+window.location ="game.html";
+
+}
+
+
+//listening
+input.addEventListener('input', validaInput);
+form.addEventListener('submit', handleSubmit);
+
